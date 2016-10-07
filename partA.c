a35e41cfc90b6dfc56e7704c1ececf95d732119f
huSin <hussainaljabri@cmail.carleton.ca>
2016-10-07 06:41:10 -0700

    Update partA.c

--- partA.c
+++ partA.c
@@ -9,14 +9,14 @@
 
 
 
-void barbar();
+void barber();
 void customer();
 void waitingRoom();
 
 
 void main(void)
 {   
-    
+    int barberFlag=0, waitingFlag=0;
     pid_t pid;
     printf("Generating a Process..\n");
     switch(pid = fork()){
@@ -25,28 +25,47 @@ void main(void)
             printf("Fork failed\n");
             break;
         case 0:
-            printf("Child process # %d is created.\n", getpid());
-            barbar();
+            printf("Child process #%d is created.\n", getpid());
+            barber();
             break;
         default:
             //sleep(1);
-            printf("Parent process # %d : generating another child.\n", getpid());
-            
+            printf("Parent process #%d : generating another child.\n", getpid());
+            switch(pid = fork()){
+                case -1:
+                    printf("Fork failed\n");
+                    break;
+                case 0:
+                    printf("Child process #% is created.\n", getpid());
+                    waitingRoom();
+                default:
+                    printf("Parent process #% is acting as customerGenerator.", getpid());
+                    customerGenerator();
+                
+                
+            }
     }
     
 }
 
-void barbar(){
-    
+void barber(){
+    printf("Barber Process #%d: ready to accept first process.", getpid());
+    if(BarberFlag=0){
+        
+        
+        
+        
+    }
 }
 
-void customer(){
-    
+void customerGenerator(){
+    printf("Customer Generator Process #%d: ready to create processes.", getpid());
+    while()
     
 }
 
 void waitingRoom(){
-    
+    printf("WaitingRoom Process #%d: ready.", getpid());
     
 }
 
